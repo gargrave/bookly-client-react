06f935a04fe1ec9a158e2b5380112af212929ce4
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-26 14:23:57 -0700

    Add base template for stateless component

---
+++ templates/StatelessComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const COMPONENT_NAME = () => {
+  return (
+    <div>
+      <h2>COMPONENT_NAME</h2>
+    </div>
+  )
+}
+
+export default COMPONENT_NAME
