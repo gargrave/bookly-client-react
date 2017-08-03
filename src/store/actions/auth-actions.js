ea98e69d71a63250475d97ab0d6700d41ce2ab18
Gabe Hargrave <gkhargrave@gmail.com>
2017-08-03 14:38:39 -0700

    Add basic login process to Redux store

--- src/store/actions/auth-actions.js
+++ src/store/actions/auth-actions.js
@@ -1,25 +1,40 @@
 import { AUTH } from '../action-types'
+import axios from 'axios'
 
-function _login () {
-  return {
-    type: AUTH.LOGIN,
-    payload: {}
-  }
+import { apiUrls } from '../../constants/urls'
+import apiHelper from '../../utils/api-helper'
+
+function _requestStart () {
+  return { type: AUTH.REQUEST_START }
+}
+
+function _requestEnd () {
+  return { type: AUTH.REQUEST_END }
+}
+
+function _login (user) {
+  return { type: AUTH.LOGIN, payload: { user } }
 }
 
 function _logout () {
-  return {
-    type: AUTH.LOGOUT,
-    payload: {}
-  }
+  return { type: AUTH.LOGOUT }
 }
 
-export function login () {
-  return dispatch => {
-    return new Promise((resolve, reject) => {
-      dispatch(_login())
-      resolve()
-    })
+export function login (user) {
+  return async dispatch => {
+    dispatch(_requestStart())
+    try {
+      const request = apiHelper.axPost(apiUrls.login, user)
+      const result = await axios(request)
+      const userData = result.data
+
+      dispatch(_login(result.data))
+      return userData
+    } catch (err) {
+      return null
+    } finally {
+      dispatch(_requestEnd())
+    }
   }
 }
 
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,14 +1,34 @@
 import { AUTH } from '../action-types'
 
-const defaultState = {}
+import userModel from '../../models/User.model'
+
+const defaultState = {
+  userRequestPending: false,
+  user: userModel.empty(),
+  token: null
+}
 
 export default function orders (state = defaultState, action) {
   switch (action.type) {
+    case AUTH.REQUEST_START:
+      return Object.assign({}, state, {
+        userRequestPending: true
+      })
+
+    case AUTH.REQUEST_END:
+      return Object.assign({}, state, {
+        userRequestPending: false
+      })
+
+    // update store with user data and token from response
     case AUTH.LOGIN:
-      return state
+      return Object.assign({}, state, {
+        user: userModel.fromAPI(action.payload.user),
+        token: action.payload.user.token
+      })
 
     case AUTH.LOGOUT:
-      return state
+      return Object.assign({}, defaultState)
 
     default:
       return state
