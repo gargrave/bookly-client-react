d7bdb751b96d8fc1fe76ec6cc41336975ad20327
Gabe Hargrave <gkhargrave@gmail.com>
2017-08-03 14:36:58 -0700

    Add API URL definitions

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,30 @@
+import env from './env'
+
+const DEV_API_ROOT_URL = 'http://localhost:3001'
+const PROD_API_ROOT_URL = 'https://bookly.gargrave.us'
+
+let apiRoot
+let restApiRoot
+
+init()
+
+// URLs for AJAX calls to the API
+export let apiUrls = {
+  // auth URLS
+  register: `${restApiRoot}/auth/register/`,
+  login: `${restApiRoot}/auth/login/`,
+  logout: `${restApiRoot}/auth/logout/`,
+  users: `${restApiRoot}/auth/users/`,
+  profiles: `${restApiRoot}/auth/profiles/`,
+  verify: `${restApiRoot}/auth/verify/`,
+  verifyResend: `${restApiRoot}/auth/verify/resend/`,
+  pwResetRequest: `${restApiRoot}/auth/passwordreset`,
+  pwResetConfirm: `${restApiRoot}/auth/passwordreset/confirm`,
+  // REST resource URLS
+  authors: `${restApiRoot}/authors/`,
+  books: `${restApiRoot}/books/`
+}
+
 // URLs for local routing (i.e. react-router)
 export let localUrls = {
   about: '/about',
@@ -16,4 +43,13 @@ export let localUrls = {
   booksList: '/books',
   bookCreate: '/books/new',
   bookDetail: '/books/:id'
+}
+
+function init () {
+  if (env.isProd()) {
+    apiRoot = PROD_API_ROOT_URL
+  } else {
+    apiRoot = DEV_API_ROOT_URL
+  }
+  restApiRoot = `${apiRoot}/api/v1`
 }
