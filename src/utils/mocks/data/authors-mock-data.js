6501e5cb308f8a799051d0c7c3fd68d78706647b
Gabe Hargrave <gkhargrave@gmail.com>
2017-08-19 18:27:36 -0700

    Add basic mock Author data

---
+++ src/utils/mocks/data/authors-mock-data.js
@@ -0,0 +1,30 @@
+export const authorsMockData = [
+  {
+    id: 8,
+    firstName: 'Aldous',
+    lastName: 'Huxley',
+    createdAt: '2017-07-01T01:16:43.881Z',
+    updatedAt: '2017-07-01T01:16:43.881Z'
+  },
+  {
+    id: 51,
+    firstName: 'Kurt',
+    lastName: 'Vonnegut',
+    createdAt: '2017-07-06T22:02:11.504Z',
+    updatedAt: '2017-07-06T22:02:11.504Z'
+  },
+  {
+    id: 59,
+    firstName: 'George R. R.',
+    lastName: 'Martin',
+    createdAt: '2017-08-17T20:47:35.232Z',
+    updatedAt: '2017-08-17T20:47:35.232Z'
+  },
+  {
+    id: 52,
+    firstName: 'Tom',
+    lastName: 'Robbins',
+    createdAt: '2017-07-06T22:02:17.051Z',
+    updatedAt: '2017-08-17T21:27:35.885Z'
+  }
+]
