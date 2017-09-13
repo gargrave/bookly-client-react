4597c7b05fa941eeadd7f960f357977c554d9419
Gabe Hargrave <gkhargrave@gmail.com>
2017-09-12 21:13:23 -0700

    Implement 'handleInputChange' and 'handleCancel' callbacks

--- src/components/Books/containers/BookCreatePage.jsx
+++ src/components/Books/containers/BookCreatePage.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { object } from 'prop-types'
 
 import bookModel from '../../../models/Book.model'
+import { localUrls } from '../../../constants/urls'
+
 import BookForm from '../components/BookForm'
 
 class BookCreatePage extends Component {
@@ -18,7 +21,12 @@ class BookCreatePage extends Component {
   }
 
   handleInputChange (event) {
-    console.log('handleInputChange')
+    const key = event.target.name
+    const book = Object.assign({}, this.state.book)
+    if (key in book) {
+      book[key] = event.target.value
+      this.setState({ book })
+    }
   }
 
   handleSubmit (event) {
@@ -28,7 +36,7 @@ class BookCreatePage extends Component {
 
   handleCancel (event) {
     event.preventDefault()
-    console.log('handleCancel')
+    this.props.history.push(localUrls.booksList)
   }
 
   render () {
@@ -47,7 +55,9 @@ class BookCreatePage extends Component {
   }
 }
 
-BookCreatePage.propTypes = {}
+BookCreatePage.propTypes = {
+  history: object
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {}
