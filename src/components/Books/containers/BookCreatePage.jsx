e872c8936a1f0a02fe3a6650cd84dd1e3f05285c
Gabe Hargrave <gkhargrave@gmail.com>
2017-09-12 21:08:19 -0700

    Setup basic structure for BookCreatePage

--- src/components/Books/containers/BookCreatePage.jsx
+++ src/components/Books/containers/BookCreatePage.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import bookModel from '../../../models/Book.model'
+import BookForm from '../components/BookForm'
+
 class BookCreatePage extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      book: bookModel.empty()
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleCancel = this.handleCancel.bind(this)
+  }
+
+  handleInputChange (event) {
+    console.log('handleInputChange')
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    console.log('handleSubmit')
+  }
+
+  handleCancel (event) {
+    event.preventDefault()
+    console.log('handleCancel')
+  }
+
   render () {
+    const { book } = this.state
     return (
       <div>
         <h2>BookCreatePage</h2>
+        <BookForm
+          book={book}
+          handleInputChange={this.handleInputChange}
+          handleSubmit={this.handleSubmit}
+          handleCancel={this.handleCancel}
+        />
       </div>
     )
   }
