4bdba472305a2239be0b337f17c30eb5ae36fa8d
Gabe Hargrave <ghargrave@workwithopal.com>
2017-10-28 13:35:40 -0700

    Clean up placeholder pages

--- src/components/Common/Home.jsx
+++ src/components/Common/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const HomeRoute = () => {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  )
-}
+const HomeRoute = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
 export default HomeRoute
--- src/components/Common/NotFound.jsx
+++ src/components/Common/NotFound.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const NotFound = () => {
-  return (
-    <div>
-      <h2>404 Not Found!</h2>
-    </div>
-  )
-}
+const NotFound = () => (
+  <div>
+    <h2>404 Not Found!</h2>
+  </div>
+)
 
 export default NotFound
