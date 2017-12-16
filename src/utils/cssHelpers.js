1e1ccac03674ee82001273a3c4e29eddc6898b2f
Gabe Hargrave <ghargrave@workwithopal.com>
2017-12-16 13:04:51 -0800

    Expand CSS helpers to build full class list

--- src/utils/cssHelpers.js
+++ src/utils/cssHelpers.js
@@ -1,6 +1,24 @@
-export const prependClass = (className) => {
+function buildClasses (prepends, statics = []) {
+  if (!Array.isArray(prepends)) {
+    prepends = [prepends]
+  }
+  if (!Array.isArray(statics)) {
+    statics = [statics]
+  }
+
+  const first = prepends.map((p) => prependClass(p)).join(' ')
+  const second = statics.length ? ` ${statics.join(' ')}` : ''
+  return `${first}${second}`
+}
+
+function prependClass (className) {
   if (!className.match(/^bookly-/)) {
     return `bookly-${className}`
   }
   return className
+}
+
+export {
+  buildClasses,
+  prependClass,
 }
