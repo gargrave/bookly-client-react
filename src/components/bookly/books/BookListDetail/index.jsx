3bdd4ebf753a0a3dae662fc00ac3aa412b766fb8
Gabe Hargrave <ghargrave@workwithopal.com>
2017-12-16 17:01:07 -0800

    Update BookListDetail to use Card component

--- src/components/bookly/books/BookListDetail/index.jsx
+++ src/components/bookly/books/BookListDetail/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { func, number, object, shape, string } from 'prop-types'
 
-import './styles.css'
+import Card from '@/components/common/Card'
 
 const BookListDetail = ({
   book,
   onClick,
 }) => {
   return (
-    <div className="book-list-detail" onClick={onClick}>
-      <p className="book-title">{book.title}</p>
-      <p className="book-author">{book.author.name}</p>
-    </div>
+    <Card
+      onClick={onClick}
+      text={book.author.name}
+      title={book.title} />
   )
 }
 
