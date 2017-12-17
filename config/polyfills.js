43a1ad6fcf8721c8f97cf5bb1c67c75c13346a90
Gabe Hargrave <ghargrave@workwithopal.com>
2017-12-17 10:02:34 -0800

    Update React to 16 (and all the other things to make it work)

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,4 +1,10 @@
-'use strict';
+'use strict'
+
+const requestAnimationFrame = require('raf/polyfill');
+const enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+enzyme.configure({ adapter: new Adapter() });
 
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
@@ -14,3 +20,9 @@ require('whatwg-fetch');
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
+
+// In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
+// We don't polyfill it in the browser--this is user's responsibility.
+if (process.env.NODE_ENV === 'test') {
+  require('raf').polyfill(global);
+}
