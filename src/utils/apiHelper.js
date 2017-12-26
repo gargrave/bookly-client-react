41e8bdc55f3a0f9c578e02742e2d3b72619867f5
Gabe Hargrave <ghargrave@workwithopal.com>
2017-12-26 10:33:11 -0800

    Fix formatting errors

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -12,9 +12,9 @@ export default {
   },
 
   /**
- * Builds a GET request for an AJAX call via axios.
- * Mostly just a shortcut to save writing everything out over and over
- */
+   * Builds a GET request for an AJAX call via axios.
+   * Mostly just a shortcut to save writing everything out over and over
+   */
   axGet(url: string, authToken?: string) {
     let req: any = {
       method: 'get',
@@ -32,8 +32,8 @@ export default {
   },
 
   /**
- * Builds a POST request for an AJAX call via axios.
- */
+   * Builds a POST request for an AJAX call via axios.
+   */
   axPost(url: string, data: Object, authToken?: string) {
     let req: any = {
       method: 'post',
@@ -52,9 +52,9 @@ export default {
   },
 
   /**
- * Builds a PUT request for an AJAX call via axios.
- * Note that this simply uses the POST method build, and then changes the method.
- */
+   * Builds a PUT request for an AJAX call via axios.
+   * Note that this simply uses the POST method build, and then changes the method.
+   */
   axPut(url: string, data: Object, authToken?: string) {
     let req: any = this.axPost(url, data, authToken);
     req.method = 'put';
@@ -62,9 +62,9 @@ export default {
   },
 
   /**
- * Builds a DELETE request for an AJAX call via axios.
- * Note that this simply uses the POST method build, and then changes the method.
- */
+   * Builds a DELETE request for an AJAX call via axios.
+   * Note that this simply uses the POST method build, and then changes the method.
+   */
   axDelete(url: string, data: Object, authToken?: string) {
     let req: any = this.axPost(url, data, authToken);
     req.method = 'delete';
